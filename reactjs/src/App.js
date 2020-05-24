18a068f607107df2228983f97f1362c1afb40ec5
danilomacb <danilo.bakun@gmail.com>
2020-05-24 11:20:39 -0300

    creating pages on react

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter,Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Inicio from './components/Inicio';
+import Sobre from './components/Sobre';
+import Informacoes from './components/Informacoes';
+import Contato from './components/Contato';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Inicio} />
+        <Route path='/sobre' exact component={Sobre} />
+        <Route path='/informacoes' exact component={Informacoes} />
+        <Route path='/contato' exact component={Contato} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ reactjs/src/components/Informacoes.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Informacoes() {
+    return <h1>Informacoes</h1>;
+}
+
+export default Informacoes;
